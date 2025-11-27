9443beab6f9abcaadb2976db1f6b7ea790ff0e5b
sachin.dwivedi <sachindwivedi1991@outlook.com>
2025-11-27 20:12:28 +0530

    Add Login with 3 attempts , menu, RepeatedInputs

---
+++ ProblemStatement1/Login.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProblemStatement1
+{
+    internal class Login
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
--- ProblemStatement1/Program.cs
+++ ProblemStatement1/Program.cs
@@ -4,7 +4,97 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Program program = new Program();
+            var result = program.AttemptedLogin();
+            if (result > 0)
+            {
+                var menus = program.GetMenuList();
+                foreach (var menu in menus)
+                {
+                    Console.WriteLine($"{menu.Position}. {menu.Name}");
+                }
+                program.RepeatedInput();
+            }
+        }
+
+        private (string name, string password) LoginScreen()
+        {
+            Console.WriteLine("==========Login Screen============");
+            Console.WriteLine("enter a username");
+            string username = Console.ReadLine();
+            Console.WriteLine("enter a password");
+            string password = Console.ReadLine();
+            return (username, password);
+        }
+        private Login GetLoginCredential()
+        {
+            var login = new Login
+            {
+                UserName = "admin",
+                Password = "1234",
+                IsActive = true
+            };
+            return login;
+        }
+        private List<Menu> GetMenuList()
+        {
+            var menus = new List<Menu>
+            {
+                new Menu { Position = 1, Name = "Add New Product" },
+                new Menu { Position = 2, Name = "Show Product" },
+                new Menu { Position = 3, Name = "Renove Product" },
+                new Menu { Position = 0, Name = "Exit" }
+
+            };
+            return menus;
+        }
+        private bool ValidateLogin(string username, string password)
+        {
+            var login = GetLoginCredential();
+            if (login.UserName == username && login.Password == password && login.IsActive)
+            {
+                return true;
+            }
+            return false;
+        }
+        private int AttemptedLogin()
+        {
+            int attempts = 3;
+            var userInput = LoginScreen();
+            while (attempts > 1)
+            {
+                if (ValidateLogin(userInput.name, userInput.password))
+                {
+                    Console.WriteLine("Login Successful!");
+                    return 1;
+                }
+                else
+                {
+                    attempts--;
+                    Console.WriteLine($"Wrong username or password and You have {attempts} attempts left.");
+                    if (attempts <= 3)
+                        LoginScreen();
+                    else
+                        return 0;
+
+                }
+            }
+            Console.WriteLine("Maximum login attempts exceeded.Exsiitng Application");
+            return 0;
+        }
+        private void RepeatedInput()
+        {
+            while (true)
+            {
+                Console.WriteLine("Enter a number");
+                string input = Console.ReadLine();
+                if (!int.TryParse(input, out int number))
+                    continue; //Ask Again
+
+                if (number == 0)
+                    break; //Exit the loop
+
+            }
         }
     }
 }
